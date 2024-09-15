0474322a148a824d49f537b75c4039baac48a061
hussein zayed <45989263+HusseinZayed@users.noreply.github.com>
2024-09-15 16:12:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
 from sklearn.preprocessing import StandardScaler
 import joblib
 from PIL import Image
 from datetime import datetime, date
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 
 
 
